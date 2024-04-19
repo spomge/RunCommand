145b7cd81912f67d14574b0dee8a587708bb614a
spomge <86209696+spomge@users.noreply.github.com>
2024-04-19 18:36:35 -0500

    Added some types and comments

--- RunCommand.lua
+++ RunCommand.lua
@@ -1,104 +1,120 @@
+--!strict
 
 if plugin == nil then
 	return
 end
 
+--// Services
 local ChangeHistoryService = game:GetService("ChangeHistoryService")
 local Selection = game:GetService("Selection")
 local RunService = game:GetService("RunService")
 local ScriptEditorService = game:GetService("ScriptEditorService")
 local HttpService = game:GetService("HttpService")
 
+--// Plugin Visual Settings
 local toolbar = plugin:CreateToolbar("CDT Studio Tools")
 
-local openScriptButton = toolbar:CreateButton("Open Script", "Open RunCommands", "rbxassetid://14978048121")
+local openScriptButton: PluginToolbarButton = toolbar:CreateButton("Open Script", "Open RunCommands", "rbxassetid://14978048121")
 local runScriptButton: PluginToolbarButton = toolbar:CreateButton("Run Script", "Create an RunCommand", "rbxassetid://14978048121")
 
 runScriptButton.ClickableWhenViewportHidden = true
 openScriptButton.ClickableWhenViewportHidden = true
 runScriptButton.Enabled = false
 
-local function GetRunCommandFolder()
-	local runCommandFolder = game:GetService("ServerScriptService"):FindFirstChild("RunCommands")
-	if not runCommandFolder then
-		runCommandFolder = Instance.new("Folder")
-		runCommandFolder.Name = "RunCommands"
-		runCommandFolder.Parent = game:GetService("ServerScriptService")
-	end
+--// Creates a folder or fetches the current one 
+local function GetRunCommandFolder(): Folder
+	
+	local runCommandFolder: Folder = game:GetService("ServerScriptService"):FindFirstChild("RunCommands") or Instance.new("Folder", game:GetService("ServerScriptService"))
+	runCommandFolder.Name = "RunCommands"
+	
 	return runCommandFolder
+	
 end
 
+--// Executes the script that is given to the function
 local function ExecuteScript(selectedScript: Script)
-	local runCommandFolder = GetRunCommandFolder()
-	local newScript = Instance.new("ModuleScript")
-	newScript.Name = HttpService:GenerateGUID()
+
+	local runCommandFolder: Folder = GetRunCommandFolder()
+	local newScript: ModuleScript = Instance.new("ModuleScript")
 	
+	newScript.Name = HttpService:GenerateGUID()
+
 	local wrapperCode = `\
-		local thread = coroutine.create(function() {selectedScript.Source} end)\
-		return thread\
+	local thread = coroutine.create(function() {selectedScript.Source} end)\
+	return thread\
 	`
-	ScriptEditorService:UpdateSourceAsync(newScript, function(oldContent)
+	ScriptEditorService:UpdateSourceAsync(newScript, function(oldContent: string)
 		return wrapperCode
 	end)
 
 	local thread: thread
-	local destroyListener = selectedScript:GetPropertyChangedSignal("Parent"):Connect(function()
+	local destroyListener: RBXScriptConnection? = selectedScript:GetPropertyChangedSignal("Parent"):Connect(function()
 		print("Destroying Module Script")
 		if thread then
 			coroutine.close(thread)
 		end
 	end)
-	
+
 	local success, runtimeError = pcall(function()
 		thread = require(newScript)
 	end)
+	
 	coroutine.resume(thread)
+	
 	while coroutine.status(thread) ~= "dead" do
 		task.wait(1)
 	end
+	
 	if destroyListener then
 		destroyListener:Disconnect()
 		destroyListener = nil
 	end
+	
 	if newScript then
 		newScript:Destroy()
 	end
+	
 end
 
 local function onRunScriptButtonClicked()
-	local selectedObjects = Selection:Get()
-	local parent = game:GetService("ServerScriptService")
+	local selectedObjects: {Instance} = Selection:Get()
+	
 	for _, selected in selectedObjects do
 		if selected:IsA("Script") then
 			ExecuteScript(selected)
 		end
 	end
+	
 end
 
 
 local function onOpenScriptButtonClicked()
-	local runCommandFolder = GetRunCommandFolder()
-	local newScript = Instance.new("Script")
-	newScript.Name = "NewCommand"
 	
+	local newScript: Script = Instance.new("Script", GetRunCommandFolder())
+	newScript.Name = "NewCommand"
+
 	newScript.Source = [[-- Click Run Script to execute!
-print("Hello World")
-]]
-	newScript.Parent = runCommandFolder
-	Selection:Set({ newScript })
+		print("Hello World")
+	]]
+	
+	Selection:Set({newScript})
 	plugin:OpenScript(newScript)
 end
 
+--// Fires respective functions on mouse clicks
 openScriptButton.Click:Connect(onOpenScriptButtonClicked)
 runScriptButton.Click:Connect(onRunScriptButtonClicked)
 
+--// When you select a different objects enable run script button
 Selection.SelectionChanged:Connect(function()
-	local selectedObjects = Selection:Get()
-	for _, selected in selectedObjects do
+	local selectedObjects: {Instance} = Selection:Get()
+	
+	for _, selected: Instance in selectedObjects do
 		if selected:IsA("Script") then
 			runScriptButton.Enabled = true
 			return
 		end
 	end
+	
 	runScriptButton.Enabled = false
 end)
